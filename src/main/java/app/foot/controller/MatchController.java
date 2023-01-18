885a2386ce104bbe12cdf29f8e078309c57fc8ea
njaina <hei.njaina.2@gmail.com>
2023-01-18 08:32:37 +0300

    feat: add controller

--- src/main/java/app/foot/controller/MatchController.java
+++ src/main/java/app/foot/controller/MatchController.java
@@ -3,25 +3,32 @@ package app.foot.controller;
 import app.foot.model.Match;
 import app.foot.model.PlayerScorer;
 import app.foot.repository.entity.MatchEntity;
+import app.foot.repository.entity.PlayerScoreEntity;
 import app.foot.service.MatchService;
+import app.foot.service.PlayerScorerService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.regex.Matcher;
+import java.util.stream.Stream;
 
 @RestController
 @AllArgsConstructor
 public class MatchController {
     private final MatchService service;
+    private final PlayerScorerService score;
 
     @GetMapping("/matches")
     public List<Match> getMatches() {
         return service.getMatches();
     }
-    /**@PostMapping("/matches/{matchId}/goals")
-    public Match addGoals(@PathVariable int matchId, @RequestBody List<PlayerScorer> scorers) {
-        scorers.forEach(validator::accept);
-        return service.addGoals(matchId, scorers);
-    }*/
+    @GetMapping("/matches/{matchId}")
+    public Stream<Match> getById(@PathVariable int id){
+        return service.getById(id);
+    }
+    @PostMapping("matches/{matchId}/scores")
+    public List<PlayerScorer> post(@RequestBody List<PlayerScoreEntity> scorer) {
+        return score.newScore(scorer);
+    }
 }
