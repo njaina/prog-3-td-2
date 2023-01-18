f15f3384ac0701aa595b9274dd168c843325c5cd
njaina <hei.njaina.2@gmail.com>
2023-01-18 05:20:34 +0300

    feat: change match service

--- src/main/java/app/foot/service/MatchService.java
+++ src/main/java/app/foot/service/MatchService.java
@@ -1,13 +1,17 @@
 package app.foot.service;
 
 import app.foot.model.Match;
+import app.foot.model.PlayerScorer;
 import app.foot.repository.MatchRepository;
 import app.foot.repository.entity.MatchEntity;
 import app.foot.repository.mapper.MatchMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -20,4 +24,10 @@ public class MatchService {
                 .map(mapper::toDomain)
                 .toList();
     }
+    public List<Match> getById(Integer id){
+        return repository.findAllById(Collections.singleton(id)).stream()
+                .map(mapper::toDomain)
+                .toList();
+    }
+
 }
