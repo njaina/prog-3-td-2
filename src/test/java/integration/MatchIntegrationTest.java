cda6c699aaf44ec4bffb0dd8def48ddfa6f43a2c
njaina <hei.njaina.2@gmail.com>
2023-02-01 18:18:13 +0300

    feat: add more test code featuring

--- src/test/java/integration/MatchIntegrationTest.java
+++ src/test/java/integration/MatchIntegrationTest.java
@@ -85,6 +85,7 @@ class MatchIntegrationTest {
         return Player.builder()
                 .id(6)
                 .name("J6")
+                .teamName(team3().getName())
                 .isGuardian(false)
                 .build();
     }
@@ -93,6 +94,7 @@ class MatchIntegrationTest {
         return Player.builder()
                 .id(3)
                 .name("J3")
+                .teamName(team2().getName())
                 .isGuardian(false)
                 .build();
     }
